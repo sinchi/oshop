931f9877abf4481be1cf680ae10e768e91e73e47
BELGHAR <ayoub.belghar@capgemini.com>
2018-04-10 12:27:25 +0100

    344 Better Object initialization

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -10,12 +10,13 @@ export class ShoppingCart {
     // tslint:disable-next-line:forin
     for (const productId in itemsMap) {
       const item = itemsMap[productId];
-      const x = new ShoppingCartItem();
-      Object.assign(x, item);
-      x.$key = productId;
+      const x = new ShoppingCartItem({
+        ...item,
+        $key: productId
+      });
+
       this.items.push(x);
 
-     // this.items.push(new ShoppingCartItem(item.product, item.quantity));
     }
   }
 
