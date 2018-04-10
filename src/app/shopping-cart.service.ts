082ea154f63a9aed826ad2735e943970b220df6e
BELGHAR <ayoub.belghar@capgemini.com>
2018-04-10 14:07:00 +0100

    Fixing a bug with updating the quantity

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -50,12 +50,18 @@ export class ShoppingCartService {
     const cartId = await this.getOrCreateCartId();
     const item$ = this.getItem(cartId, product.$key);
     item$.take(1).subscribe(item => {
-      item$.update({
-        title: product.title,
-        imageUrl: product.imageUrl,
-        price: product.price,
-        quantity: (item.quantity || 0) + change
-       });
+      const quantity = (item.quantity || 0) + change;
+      if (quantity === 0) {
+        item$.remove();
+      } else {
+        item$.update({
+          title: product.title,
+          imageUrl: product.imageUrl,
+          price: product.price,
+          quantity: (item.quantity || 0) + change
+         });
+      }
+
     });
   }
 
