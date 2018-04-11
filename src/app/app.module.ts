fabb5477192cd76d33ba22f2c718214f7b20edbb
BELGHAR <ayoub.belghar@capgemini.com>
2018-04-11 09:51:45 +0100

    add order service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { ProductFilterComponent } from './products/product-filter/product-filter
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ShoppingCartService } from './shopping-cart.service';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
+import { OrderService } from './order.service';
 
 
 @NgModule({
@@ -89,7 +90,8 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
     UserService,
     CategoryService,
     ProductService,
-    ShoppingCartService
+    ShoppingCartService,
+    OrderService
    ],
   bootstrap: [AppComponent]
 })
--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Subscription } from 'rxjs';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  shipping = {};
+  cart: ShoppingCart;
+  subscription: Subscription;
 
-  ngOnInit() {
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private orderService: OrderService
+  ) { }
+
+  async ngOnInit() {
+    const cart$ = await this.shoppingCartService.getCart();
+    this.subscription = cart$.subscribe(cart => this.cart = cart;)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  placeOrder() {
+    const order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(i => {
+        return {
+          product: {
+            title: i.title,
+            imageUrl: i.imageUrl,
+            price: i.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        };
+      })
+    };
+
+    this.orderService.storeOrder(order);
   }
 
 }
